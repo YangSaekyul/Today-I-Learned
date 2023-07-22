27ffc652e9801f412564764d28c2bc92a53c11b0
Saekyul <saekyulyang@gmail.com>
2023-07-22 11:24:36 +0900

    230722 - codingtest

--- 자료구조,알고리즘/test.js
+++ 자료구조,알고리즘/test.js
@@ -1,6 +1,18 @@
 let fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim();
+let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-let lower = input.toLowerCase();
+let filteredData = input.filter((element) => {
+  element.split(" ")[2] !== "P";
+});
 
-console.log(lower);
+let result = filteredData.map((element) => {
+  let data = element.split(" ");
+  let object = {
+    과목: data[0],
+    점수: data[1],
+    등급: data[2],
+  };
+  return object;
+});
+
+console.log(result);
--- 자료구조,알고리즘/어려웠던 문제/백준/2941.js
+++ 자료구조,알고리즘/어려웠던 문제/백준/2941.js
@@ -8,3 +8,24 @@ for (let alphabet of changeAlphabet) {
 }
 
 console.log(input.length);
+
+// 예제 입력 1
+// ljes=njak
+// 예제 출력 1
+// 6
+// 예제 입력 2
+// ddz=z=
+// 예제 출력 2
+// 3
+// 예제 입력 3
+// nljj
+// 예제 출력 3
+// 3
+// 예제 입력 4
+// c=c=
+// 예제 출력 4
+// 2
+// 예제 입력 5
+// dz=ak
+// 예제 출력 5
+// 3
