5931503506d3491817d603bd235a570b9c08eed8
Saekyul <saekyulyang@gmail.com>
2023-07-23 22:42:17 +0900

    -

--- 자료구조,알고리즘/codingtest.js
+++ 자료구조,알고리즘/codingtest.js
@@ -1,10 +1,2 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().trim();
-
-let changeAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
-
-for (let alphabet of changeAlphabet) {
-  input = input.split(alphabet).join("*");
-}
-
-console.log(input.length);
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
--- 자료구조,알고리즘/test.js
+++ 자료구조,알고리즘/test.js
@@ -1,18 +1,2 @@
 let fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
-
-let filteredData = input.filter((element) => {
-  element.split(" ")[2] !== "P";
-});
-
-let result = filteredData.map((element) => {
-  let data = element.split(" ");
-  let object = {
-    과목: data[0],
-    점수: data[1],
-    등급: data[2],
-  };
-  return object;
-});
-
-console.log(result);
